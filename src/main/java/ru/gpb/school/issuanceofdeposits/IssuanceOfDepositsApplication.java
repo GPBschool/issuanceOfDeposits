95621fc501c34774a1c333eefdcd8d21746ef76c
alekseygl1978 <44414087+alekseygl1978@users.noreply.github.com>
2022-01-27 09:51:01 +0300

    init project

---
+++ src/main/java/ru/gpb/school/issuanceofdeposits/IssuanceOfDepositsApplication.java
@@ -0,0 +1,13 @@
+package ru.gpb.school.issuanceofdeposits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuanceOfDepositsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuanceOfDepositsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gpb/school/issuanceofdeposits/IssuanceOfDepositsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpb.school.issuanceofdeposits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuanceOfDepositsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
